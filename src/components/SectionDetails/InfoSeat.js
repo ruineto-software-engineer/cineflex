481dc18b7074fc43c3d29a9f0645594e1c6a3d6d
Rui Neto <ruineto11@gmail.com>
2021-12-23 14:16:39 -0300

    Implementação da função de validação de cpf

--- src/components/SectionDetails/InfoSeat.js
+++ src/components/SectionDetails/InfoSeat.js
@@ -74,34 +74,44 @@ export default function InfoSeat(props) {
           alert("Reveja o campo de CPF, não pode ser menor do que 14 caracteres.");
           return;
         }else{
+          if(cpfValidation(inputInfo.cpf)){
+            setInputCondition("disabled-input");
+            setConfirmButtonCondition("disabled-button");
+            setEditButtonCondition("");
+      
+            setInputInfo({ idAssento: '', nome: "", cpf: "" });
+            props.seatStage({
+              ids: [ ...props.seatValue.ids ],
+              compradores: [
+                ...props.seatValue.compradores,
+                inputInfo
+              ]
+            });
+          }else{
+            alert("O CPF informado é inválido, por gentileza, reveja o campo.");
+            return;
+          }
+        }
+      }else{
+        if(cpfValidation(inputInfo.cpf)){
           setInputCondition("disabled-input");
           setConfirmButtonCondition("disabled-button");
           setEditButtonCondition("");
-    
-          setInputInfo({ idAssento: '', nome: "", cpf: "" });
+  
           props.seatStage({
             ids: [ ...props.seatValue.ids ],
-            compradores: [
-              ...props.seatValue.compradores,
-              inputInfo
-            ]
+            compradores: props.seatValue.compradores.map((comprador, index) => {
+              if(comprador.idAssento === props.seatNumber){
+                return props.seatValue.compradores[indexBuyer] = inputInfo;
+              }else{
+                return props.seatValue.compradores[index];
+              }
+            })
           });
+        }else{
+          alert("O CPF informado é inválido, por gentileza, reveja o campo.");
+          return;
         }
-      }else{
-        setInputCondition("disabled-input");
-        setConfirmButtonCondition("disabled-button");
-        setEditButtonCondition("");
-
-        props.seatStage({
-          ids: [ ...props.seatValue.ids ],
-          compradores: props.seatValue.compradores.map((comprador, index) => {
-            if(comprador.idAssento === props.seatNumber){
-              return props.seatValue.compradores[indexBuyer] = inputInfo;
-            }else{
-              return props.seatValue.compradores[index];
-            }
-          })
-        });
       }
     }
   }
@@ -148,4 +158,45 @@ function FromGroup(props) {
         </div>
     </Fragment>
   );
+}
+
+function cpfValidation(cpf) {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf === '') return false;
+
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length !== 11 || 
+		cpf === "00000000000" || 
+		cpf === "11111111111" || 
+		cpf === "22222222222" || 
+		cpf === "33333333333" || 
+		cpf === "44444444444" || 
+		cpf === "55555555555" || 
+		cpf === "66666666666" || 
+		cpf === "77777777777" || 
+		cpf === "88888888888" || 
+		cpf === "99999999999")
+			return false;
+
+	// Valida 1o digito	
+	let add = 0;
+  let rev;	
+	for (let i = 0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev === 10 || rev === 11)		
+			rev = 0;	
+		if (rev !== parseInt(cpf.charAt(9)))		
+			return false;	
+
+	// Valida 2o digito	
+	add = 0;	
+	for (let i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev === 10 || rev === 11)	
+		rev = 0;	
+	if (rev !== parseInt(cpf.charAt(10)))
+		return false;		
+	return true;   
 }
